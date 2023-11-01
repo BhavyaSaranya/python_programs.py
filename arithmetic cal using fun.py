d082bb10a98889ffd6e73f0e84d59924d62e3577
BhavyaSaranya <147613606+BhavyaSaranya@users.noreply.github.com>
2023-11-01 15:49:14 +0530

    Add files via upload

---
+++ arithmetic cal using fun.py
@@ -0,0 +1,48 @@
+#Arithmetic calculations using functions
+#addition
+def add(num1,num2):
+    return num1+num2
+num1=int(5)
+num2=int(7)
+print(add(num1,num2))
+
+#subtraction
+def sub(num1,num2):
+    return num1+num2
+num1=int(25)
+num2=int(20)
+print(sub(num1,num2))
+
+#multiplication
+def mul(num1,num2):
+    return num1*num2
+num1=int(4)
+num2=int(5)
+print(mul(num1,num2))
+
+#division
+
+def div(num1,num2):
+    return num1/num2
+num1=20
+num2=2
+print(div(num1,num2))
+
+#module division
+def module(num1,num2):
+    return num1%num2
+num1=10
+num2=5
+print(module(num1,num2))
+
+#floor division
+def fldiv(num1,num2):
+    return num1//num2
+num1=10
+num2=5
+print(fldiv(num1,num2))
+
+
+
+
+
